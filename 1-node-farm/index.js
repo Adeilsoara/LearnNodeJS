326626d07d1278b4eccb67508ecbed34585c02b8
Adeilsoara <adeilsonaragao@hotmail.com>
2022-06-21 16:18:18 -0300

    Asynchronous mode

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,10 +1,18 @@
+/*Blocking Synchronous mode */
+
 const fs = require('fs')
 
-const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8')
+/* const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8')
 console.log(textIn)
 
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
 fs.writeFileSync('./starter/txt/output.txt', textOut)
-console.log('Text written')
+console.log('Text written') */
 /* const hello = 'Hello World'
 console.log(hello) */
+
+/* Non-blocking, asynchronous way */
+fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data) =>{
+    console.log(data)
+})
+console.log("Will read file!")
