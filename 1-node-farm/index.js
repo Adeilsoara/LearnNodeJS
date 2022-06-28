f366f4ae24c61bf4ad89fd16ef6d39b337080044
Adeilsoara <adeilsonaragao@hotmail.com>
2022-06-28 16:13:57 -0300

    Correction of bug

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -33,7 +33,7 @@ const replaceTemplate = (temp, product) =>{
     output = output.replace(/{%ID%}/g, product.id)
 
 
-    if(!product.organic) output = temp.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
     return output
 }
 
