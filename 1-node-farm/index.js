7233e9eb98689a0f60284646b0042f09e07c9604
Adeilsoara <adeilsonaragao@hotmail.com>
2022-06-28 17:01:11 -0300

    Page product

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -46,10 +46,11 @@ const dataObj = JSON.parse(data)
 
 
 const server = http.createServer((req, res) =>{
-    const pathName = req.url
+
+    const { query, pathname } = url.parse(req.url, true)
    
     //overview page
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, {'Content-type' : 'text/html'})
 
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
@@ -58,11 +59,14 @@ const server = http.createServer((req, res) =>{
         res.end(output)
     
     // product page
-    }else if(pathName === '/product'){
-        res.end('This is the Product!')
+    }else if(pathname === '/product'){
+        res.writeHead(200, {'Content-type' : 'text/html'})
+        const product = dataObj[query.id]
+        const output = replaceTemplate(tempProduct, product)
+        res.end(output)
     
     //API
-    }else if (pathName === '/api'){
+    }else if (pathname === '/api'){
         res.writeHead(200, {'Content-type' : 'application/json'})
         res.end(data)
     
