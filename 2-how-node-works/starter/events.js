b0b8ca233fef1d75750600c4c62049857a17d5d0
Adeilsoara <adeilsonaragao@hotmail.com>
2022-07-06 09:17:07 -0300

    Requests for Server

--- 2-how-node-works/starter/events.js
+++ 2-how-node-works/starter/events.js
@@ -1,5 +1,6 @@
 const EventEmitter = require("events")
 
+const http = require('http')
 /* const myEmitter = new EventEmitter() */
 
 class Sales extends EventEmitter {
@@ -23,3 +24,24 @@ myEmitter.on('newSale', stock => {
 })
 
 myEmitter.emit("newSale", 9)
+
+/////////////////////
+
+const server = http.createServer()
+
+server.on("request", (req, res) => {
+    console.log("Request received")
+    res.end("Request received")
+})
+
+server.on("request", (req, res) => {
+    res.end("Another request 😁")
+})
+
+server.on("close", () => {
+    console.log("Server closed")
+})
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Waiting for requests...")
+})
